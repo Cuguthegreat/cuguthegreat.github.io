da7041f39d5f2b4d2885cf9607e6deec18b0e523
Cuguthegreat <60847857+Cuguthegreat@users.noreply.github.com>
2020-02-25 22:20:27 +0100

    add hoerko

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ export const protectedEntities = [
     '5e409e27ee7ee6001715c7b4',
     '5e409e27ee7ee6001715c7b3',
     '5e409e27ee7ee6001715c7b2',
+    '5e4e7af09683870017ea8a89'
 ];
 export const defaultSquareColor = 'a9d271';
 
