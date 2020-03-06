7c95f0b22faa92555674aa6aa02d038ffbe4053d
Cuguthegreat <60847857+Cuguthegreat@users.noreply.github.com>
2020-03-06 01:42:17 +0100

    Add text to rat counter

--- src/init.js
+++ src/init.js
@@ -43,8 +43,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = "Alles für die Ratte!" + "<br />" + "Alles für den Club!" + "<br />" +
+  "Unser Leben für den Nager!" + "<br />" + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
